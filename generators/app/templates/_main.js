23a3e79c7a5baa8f870b4f8102f191b963302fbf
Andy Thompson <osilat3@gmail.com>
2016-04-09 18:46:21 -0400

    cleaned up javascript file paths

--- generators/app/templates/_main.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by osila_000 on 11/7/2014.
- */
---
+++ generators/webapp/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var _global = (function() {
+  return this;
+})();
+
+var app;
+
+_global.WEBSITE = _global.WEBSITE || {};
+_global.WEBSITE = app;
+
+//application functions go here
+
+app.loadingView = function() {
+
+};
+
+app.loadContent = function() {
+
+};
+
+module.exports = _global;
